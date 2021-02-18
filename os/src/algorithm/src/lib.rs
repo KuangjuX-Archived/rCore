e1f081090949fca568bd5b57e6b5265186f04898
KuangjuX <qcx@tju.edu.cn>
2021-02-18 04:20:58 -0800

    update

--- os/src/algorithm/src/lib.rs
+++ os/src/algorithm/src/lib.rs
@@ -2,6 +2,19 @@
 //!
 //! 以及有多种实现，会留作业的数据结构
 #![no_std]
+#![feature(drain_filter)]
+
+extern crate alloc;
+
+mod allocator;
+mod scheduler;
+
+pub use allocator::*;
+pub use scheduler::*;
+//! 一些可能用到，而又不好找库的数据结构
+//!
+//! 以及有多种实现，会留作业的数据结构
+#![no_std]
 
 extern crate alloc;
 
--- os/src/algorithm/target/rls/riscv64imac-unknown-none-elf/debug/deps/algorithm-e1a774661deccca1.d
+++ os/src/algorithm/target/rls/riscv64imac-unknown-none-elf/debug/deps/algorithm-e1a774661deccca1.d
@@ -1,9 +1,12 @@
-/home/kuangjux/rCore/os/src/algorithm/target/rls/riscv64imac-unknown-none-elf/debug/deps/algorithm-e1a774661deccca1.rmeta: src/lib.rs src/allocator/mod.rs src/allocator/stacked_allocator.rs src/allocator/bitmap_vector_allocator.rs src/allocator/segment_tree_allocator.rs
+/home/kuangjux/rCore/os/src/algorithm/target/rls/riscv64imac-unknown-none-elf/debug/deps/algorithm-e1a774661deccca1.rmeta: src/lib.rs src/allocator/mod.rs src/allocator/stacked_allocator.rs src/allocator/bitmap_vector_allocator.rs src/allocator/segment_tree_allocator.rs src/scheduler/mod.rs src/scheduler/fifo_scheduler.rs src/scheduler/hrrn_scheduler.rs
 
-/home/kuangjux/rCore/os/src/algorithm/target/rls/riscv64imac-unknown-none-elf/debug/deps/algorithm-e1a774661deccca1.d: src/lib.rs src/allocator/mod.rs src/allocator/stacked_allocator.rs src/allocator/bitmap_vector_allocator.rs src/allocator/segment_tree_allocator.rs
+/home/kuangjux/rCore/os/src/algorithm/target/rls/riscv64imac-unknown-none-elf/debug/deps/algorithm-e1a774661deccca1.d: src/lib.rs src/allocator/mod.rs src/allocator/stacked_allocator.rs src/allocator/bitmap_vector_allocator.rs src/allocator/segment_tree_allocator.rs src/scheduler/mod.rs src/scheduler/fifo_scheduler.rs src/scheduler/hrrn_scheduler.rs
 
 src/lib.rs:
 src/allocator/mod.rs:
 src/allocator/stacked_allocator.rs:
 src/allocator/bitmap_vector_allocator.rs:
 src/allocator/segment_tree_allocator.rs:
+src/scheduler/mod.rs:
+src/scheduler/fifo_scheduler.rs:
+src/scheduler/hrrn_scheduler.rs:
