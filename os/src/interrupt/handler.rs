5560e6ce9d1bebfa03ff9e6802b07caa3b0cd570
KuangjuX <qcx@tju.edu.cn>
2021-02-19 00:59:44 -0800

    update

--- os/src/interrupt/handler.rs
+++ os/src/interrupt/handler.rs
@@ -1,7 +1,7 @@
 use super::context::Context;
 use super::timer;
-// use crate::fs::STDIN;
-// use crate::kernel::syscall_handler;
+use crate::fs::STDIN;
+use crate::kernel::syscall_handler;
 use crate::memory::*;
 use crate::process::PROCESSOR;
 use crate::sbi::console_getchar;
@@ -12,18 +12,19 @@ use riscv::register::{
 
 global_asm!(include_str!("./interrupt.asm"));
 
-// init interrupt
-// write tne entry of __interrupt into stvec and start interrupt function
+/// 初始化中断处理
+///
+/// 把中断入口 `__interrupt` 写入 `stvec` 中，并且开启中断使能
 pub fn init() {
-    unsafe{
+    unsafe {
         extern "C" {
-            // Entry of `interrupt_asm`
+            /// `interrupt.asm` 中的中断入口
             fn __interrupt();
         }
-
-        // Use Direct Mode, set entry of interrupt `__interrupt`
+        // 使用 Direct 模式，将中断入口设置为 `__interrupt`
         stvec::write(__interrupt as usize, stvec::TrapMode::Direct);
 
+        // 开启外部中断使能
         sie::set_sext();
 
         // 在 OpenSBI 中开启外部中断
@@ -37,92 +38,67 @@ pub fn init() {
     }
 }
 
-
-/// Entry of interrupt handler
-/// `interrupt.asm` first save the register to Context, 
-/// which is passed to this function as a parameter along with scause and stval
-/// The specific interrupt type needs to be inferred according to scause, 
-/// and then processed separately
+/// 中断的处理入口
+///
+/// `interrupt.asm` 首先保存寄存器至 Context，其作为参数和 scause 以及 stval 一并传入此函数
+/// 具体的中断类型需要根据 scause 来推断，然后分别处理
 #[no_mangle]
-pub fn handle_interrupt(context: &mut Context, scause: Scause, stval: usize) -> *mut Context{
-    // panic!("Interrupted: {:?}", scause.cause());
-    // context.sepc = 0;
+pub fn handle_interrupt(context: &mut Context, scause: Scause, stval: usize) -> *mut Context {
+    // 首先检查线程是否已经结束（内核线程会自己设置标记来结束自己）
     {
         let mut processor = PROCESSOR.lock();
         let current_thread = processor.current_thread();
-        if current_thread.as_ref().unwrap().dead {
-            println!("thread {} exit.", current_thread.id);
+        if current_thread.as_ref().inner().dead {
+            println!("thread {} exit", current_thread.id);
             processor.kill_current_thread();
             return processor.prepare_next_thread();
         }
     }
-
+    // 根据中断类型来处理，返回的 Context 必须位于放在内核栈顶
     match scause.cause() {
-        // breakpoint interrupt(ebark)
+        // 断点中断（ebreak）
         Trap::Exception(Exception::Breakpoint) => breakpoint(context),
-        // clock interrupt
+        // 系统调用
+        Trap::Exception(Exception::UserEnvCall) => syscall_handler(context),
+        // 时钟中断
         Trap::Interrupt(Interrupt::SupervisorTimer) => supervisor_timer(context),
-        // Visit a non-existent address
-        Trap::Exception(Exception::LoadFault) => load_falut(context, scause, stval),
-        // Outer Interrupt
-        // Trap::Interrupt(Interrupt::SupervisorExternal) => supervisor_external(context),
-        // Other Situations: Stop current thread
+        // 外部中断（键盘输入）
+        Trap::Interrupt(Interrupt::SupervisorExternal) => supervisor_external(context),
+        // 其他情况，无法处理
         _ => fault("unimplemented interrupt type", scause, stval),
-    };
+    }
 }
 
-/// Set ebreak breakpoint
-/// 
-/// Continue execution, where `sepc` is 
-/// increased by 2 bytes to skip the current `ebreak` instruction
-fn breakpoint(context: &mut Context) -> *mut Context{
+/// 处理 ebreak 断点
+///
+/// 继续执行，其中 `sepc` 增加 2 字节，以跳过当前这条 `ebreak` 指令
+fn breakpoint(context: &mut Context) -> *mut Context {
     println!("Breakpoint at 0x{:x}", context.sepc);
     context.sepc += 2;
-    // context.sepc = 0;
-    context;
+    context
 }
 
-
-/// Handler clock interrupt
-/// 
-/// Currently only counting in the [`timer`] module
-fn supervisor_timer(context: &mut Context) -> *mut Context{
+/// 处理时钟中断
+fn supervisor_timer(context: &mut Context) -> *mut Context {
     timer::tick();
     PROCESSOR.lock().park_current_thread(context);
-    PROCESSOR.lock().prepare_next_thread();
-    context;
+    PROCESSOR.lock().prepare_next_thread()
 }
-
-// Handler Load Fault
-fn load_falut(context: &mut Context, scause: Scause, stval: usize) -> *mut Context{
-    if (stval == 0){
-        println!("Success!")
-    }
-    panic!(
-        "Interrupt: {:?}\n{:x?}\nstval: {:x}",
-        scause.cause(),
-        context,
-        stval
-    );
-    context;
-}
-
 
 /// 处理外部中断，只实现了键盘输入
-// fn supervisor_external(context: &mut Context) -> *mut Context {
-//     let mut c = console_getchar();
-//     if c <= 255 {
-//         if c == '\r' as usize {
-//             c = '\n' as usize;
-//         }
-//         STDIN.push(c as u8);
-//     }
-//     context
-// }
+fn supervisor_external(context: &mut Context) -> *mut Context {
+    let mut c = console_getchar();
+    if c <= 255 {
+        if c == '\r' as usize {
+            c = '\n' as usize;
+        }
+        STDIN.push(c as u8);
+    }
+    context
+}
 
-
-/// An unresolved exception occurred
-fn fault(msg: &str, scause: Scause, stval: usize) -> *mut Context{
+/// 出现未能解决的异常，终止当前线程
+fn fault(msg: &str, scause: Scause, stval: usize) -> *mut Context {
     println!(
         "{:#x?} terminated: {}",
         PROCESSOR.lock().current_thread(),
